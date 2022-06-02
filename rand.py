c2a82d30a3d403c26475bd787cdef5c99908850a
Showndharyaa <showndharyaa27@gmail.com>
2022-06-02 11:58:21 -0500

    Random sequence file

---
+++ rand.py
@@ -0,0 +1,12 @@
+from random import randint
+from time import sleep
+
+x = randint(0,10)
+
+def randomPattern(self):
+    if x > 0:
+        print(x)
+        sleep(x)
+
+if __name__ == "__main__":
+    randomPattern(x)
